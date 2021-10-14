5e8defb76c3b1ca9098b6647ec07251cf34f0663
Ryan Taylor <RyanCTaylor95@gmail.com>
2021-10-14 12:09:06 +1000

    Test code for server side/client side check

--- pages/index.vue
+++ pages/index.vue
@@ -4,12 +4,28 @@
     <NuxtLink to="/">Home Page</NuxtLink>
     <NuxtLink to="/about">About page</NuxtLink> 
 
+    <p>
+      This page is rendered on the <strong>{{ rendering }}</strong>
+    </p>
+    <p v-if="rendering === 'server'">
+      First load or hard refresh is done server side.
+    </p>
+    <p v-if="rendering === 'client'">
+      Navigation done on client side
+    </p>
 
+    <NuxtLink to="/about">About Page</NuxtLink>
   </main>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  asyncData() {
+    return {
+      rendering: process.server ? 'server' : 'client'
+    }
+  }
+})
 </script>
