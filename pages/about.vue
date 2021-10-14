fdb89a404f02f0757bd8a41897b0196a0803ff79
Ryan Taylor <RyanCTaylor95@gmail.com>
2021-10-14 11:51:26 +1000

    Init commit

---
+++ pages/about.vue
@@ -0,0 +1,9 @@
+<template>  
+  <main>
+    <h3>About Page</h3>
+    <NuxtLink to="/">Home Page</NuxtLink>
+    <NuxtLink to="/about">About page</NuxtLink> 
+
+
+  </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>  
+  <main>
+    <h3>Home Page</h3>
+    <NuxtLink to="/">Home Page</NuxtLink>
+    <NuxtLink to="/about">About page</NuxtLink> 
+
+
+  </main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
